2870c6d913880cdfd2220da54a56bb71a6465b49
Mirkomil113 <shukurovmirkomil12@gmail.com>
2024-09-26 21:10:27 +0500

    added main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+var studentAge=3;
+var votingAge=19;
+
+if (studentAge>=votingAge) {
+   console.log("You Can Vote");
+}
+
+else if (studentAge==votingAge) {
+   console.log("You Can Vote");
+}
+else {
+   console.log("Come Back Later");
+}
+
+
+
+
+
+var studentScore=89;
+
+if (studentScore>=90) {
+   console.log("You got A");
+}
+else if (studentScore<90 && studentScore>=70) {
+   console.log("You got B");
+}
